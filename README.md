a6aec24eee0c20c29c8cc61ce4c228d0785916cb
aelias-thepacer <aelias.thepacer@gmail.com>
2024-12-06 06:35:54 -0700

    Finished working copy and updated README.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# auto-readme-generator
+# Automatic README Generator
+
+## Description
+
+This program automatically generates a styled README file for any use.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Download the repo and run npm i in order to install the necessary dependencies.
+
+## Usage
+
+Run node index.js in order to run the program. Once running, answer the prompts the program gives you. These will be the EXACT words on the readme, so answer carefully. The program will then automatically create a readme based on the input you have given.
+
+## Contributing
+
+If you want to contribute, contact me at my email below.
+
+## Tests
+
+No tests necessary.
+
+## Questions
+
+My Github profile is aelias-thepacer. You can also reach me at aelias.thepacer@gmail.com if you have additional questions.
--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your README title?", "What is your description?", "What are the installation instructions for this project?", "What is the usage information?", "What are the guidelines for contributing?", "What are the instructions for testing?", "What license would you like to use?", "What is your Github username?", "What is your email adress?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log("File Written");
+});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            name: "title",
+            type: "input",
+            message: questions[0]
+        },
+            
+        {
+            name: "description",
+            type: "input",
+            message: questions[1]
+        },
+    
+        {
+            name: "installation",
+            type: "input",
+            message: questions[2]
+        },
+    
+        {
+            name: "usage",
+            type: "input",
+            message: questions[3]
+        },
+    
+        {
+            name: "contribute",
+            type: "input",
+            message: questions[4]
+        },
+
+        {
+            name: "testing",
+            type: "input",
+            message: questions[5]
+        },
+
+        {
+            name: "license",
+            type: "list",
+            message: questions[6],
+            choices: ["MIT", "Apache 2.0", "GPL", "BSD (2-clause)", "BSD (3-clause)", "BSD (4-clause)"]
+        },
+
+        {
+            name: "gitHub",
+            type: "input",
+            message: questions[7]
+        },
+
+        {
+            name: "email",
+            type: "input",
+            message: questions[8]
+        }
+    ])
+    .then((data) => {
+        writeToFile("READMEtest.md", generateMarkdown(data))
+})
+}
 
 // Function call to initialize app
 init();
